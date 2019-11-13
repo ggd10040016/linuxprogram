1f47677a3039c6a776de84295445f6f5a3547f77
ggd <1275379053@qq.com>
2019-11-13 00:27:07 -0800

    d

---
+++ week9/1-3.c
@@ -0,0 +1,41 @@
+#include"my.h"
+
+static void callback1()
+{
+ printf("-----callback1----\n");
+}
+static void callback2()
+{
+ printf("-----callback2----\n");
+}
+static void __attribute__((constructor)) before_main(void)
+{
+ printf("------running before main------\n");
+}
+static void __attribute__((destructor)) after_main(void)
+{
+ printf("------running after main------\n");
+}
+int main(void)
+{
+ atexit(callback1);
+ atexit(callback2);
+ FILE *fp;
+ char buf[]={"fputs use full buffer!"};
+ if((fp=fopen("test2.dat","w+"))==NULL)
+{
+ perror("failed to fopen!\n");
+ return -1;
+}
+ if(fputs(buf,fp)==EOF)
+{
+ perror("failed to fputs!\n");
+ return -1;
+}
+ printf("------running in main--\n");
+ printf("printf use line buffer");
+//while(1);
+// _exit(0);
+exit(0);
+//return 0;
+}
