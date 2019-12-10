28dff046dd90092c8869593a1c3eae9868cef37f
ggd <1275379053@qq.com>
2019-12-09 16:40:20 -0800

    a

---
+++ week14/my.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<wait.h>
+#include<fcntl.h>
+#include<string.h>
+#include<signal.h>
+#include<sys/time.h>
+#include<sys/param.h>
+#include<pthread.h>
+
