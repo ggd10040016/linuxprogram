93be61bfbb2e595118e6663553c8aa2b18747d13
ggd <1275379053@qq.com>
2019-12-16 17:18:13 -0800

    a

---
+++ week15/my.h
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<wait.h>
+#include<fcntl.h>
+#include<string.h>
+#include<signal.h>
+#include<sys/time.h>
+#include<sys/param.h>
+#include<pthread.h>
+#include<limits.h>
+#include<errno.h>
+#define LOOP 10000000
+#define NUM 4
+#define TN 4
